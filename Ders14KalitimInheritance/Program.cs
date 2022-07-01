67d8a4f1648be65a08d3d7e596128836c1a7a81a
Arı Bilgi <aripendik@outlook.com>
2022-07-01 21:26:55 +0300

    1-7-2022 interfaces

--- Ders14KalitimInheritance/Program.cs
+++ Ders14KalitimInheritance/Program.cs
@@ -18,6 +18,26 @@
         public string Marka { get; set; }
         public string Model { get; set; }
     }
+
+    public class AtaSinif //Miras alınacak olan baba sınıf
+    {//ctor tab tab
+        public AtaSinif() //Kurucu method, kurucu metotlar classlar ile aynı isimde olur ve class new lenerek bir yerde kullanılmak istenirse ilk çalışan metot kurucu metottur, sonra class içindeki diğer metotlar çalışır
+        {
+            Console.WriteLine("Ata sınıf kurucu metot çalıştı");
+        }
+        public void Yaz()
+        {
+            Console.WriteLine("Ata sınıftaki yaz metodu çalıştı");
+        }
+    }
+    public class OgulSinif : AtaSinif
+    {
+        public OgulSinif()
+        {
+            Console.WriteLine("Oğul sınıf kurucu metot çalıştı");
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
@@ -28,7 +48,57 @@
             otomobil.Marka = "Tofaşk";
             otomobil.Model = "Doğan SLX";
             otomobil.AracKornasi();
+            Console.WriteLine($"Araç Bilgileri : Araç Türü : {otomobil.AracTuru} - Markası : {otomobil.Marka} - Modeli : {otomobil.Model}");
+            Console.WriteLine();
 
+            OgulSinif ogul = new();
+            ogul.Yaz();
+
+            Console.WriteLine();
+            Console.WriteLine("Polimorpizm-Çok Biçimlilik");
+
+            Cizici[] birCizici = new Cizici[4];
+            birCizici[0] = new DogruCiz();
+            birCizici[1] = new DaireCiz();
+            birCizici[2] = new KareCiz();
+            birCizici[3] = new Cizici();
+
+            foreach (var item in birCizici)
+            {
+                item.Ciz();
+            }
+
+        }
+    }
+
+    //Polimorfizm (Çok biçimlilik) : Bir fonksiyonun normal davranışının ezilerek farklı bir çıktı vermesini sağladığımız yöntem
+    public class Cizici
+    {
+        public virtual void Ciz() //virtual ibaresi ile ciz metodumuzu ezilebilir hale getirdik
+        {
+            Console.WriteLine("Çizici");
+        }
+    }
+
+    public class DogruCiz : Cizici
+    {
+        public override void Ciz() //override ibaresi ile ciz metodumuzu ezerek farklı çıktı üretmesini sağladık
+        {
+            Console.WriteLine("Düz çizgi");
+        }
+    }
+    public class DaireCiz : Cizici
+    {
+        public override void Ciz()
+        {
+            Console.WriteLine("Daire çizgi");
+        }
+    }
+    public class KareCiz : Cizici
+    {
+        public override void Ciz()
+        {
+            Console.WriteLine("Kare çizgi");
         }
     }
 }
---
+++ Ders16InterfacesArayuzler/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ders16InterfacesArayuzler
+{
+    interface OrnekArayuz // Örnek interface
+    {
+        public int Id { get; set; } // interface içindeki prop
+    }
+
+    interface IDemo // interface isimleri I ile başlamalıdır
+    {
+        void Goster(); // Interface içerisinde örnek bir metot imzası
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Interfaces - Arayüzler!");
+            // C# dilinde arayüz soyut bir veri tipidir. Sınıfa benzer şekilde bildirilir ancak içerdiği öğelerin tanımı değil yalnızca imzaları vardır.
+
+        }
+    }
+}
